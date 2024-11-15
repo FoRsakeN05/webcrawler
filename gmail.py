d7d0a4970e3c893b2cf7f7f3c9b7db8f152fc41b
LuisFernando200 <123518880+LuisFernando200@users.noreply.github.com>
2024-11-14 19:34:44 -0600

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -13,40 +13,32 @@ driver.get("https://mail.google.com/")
 
 try:
     correo = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.ID, "identifierId")))
-
     correo.send_keys("webtrabajos0@gmail.com")
     correo.send_keys(Keys.ENTER)
     
     contra = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.NAME, "Passwd")))
-
     contra.send_keys("Webcraler23")
     contra.send_keys(Keys.RETURN)
-    
+
     boton_mas = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'J-Ke') and contains(@class, 'n4')]"))
     )
     boton_mas.click()
 
-    boton_todos = WebDriverWait(driver, 20).until(
+    boton_noleidos = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, "//a[text()='Todos']"))
     )
-    boton_todos.click()
+    boton_noleidos .click()
 
     boton_no_leidos = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, "//a[text()='No leídos']"))
+        EC.element_to_be_clickable((By.XPATH, "//span[@class='H5 bwo' and text()='No leídos']"))
     )
     boton_no_leidos.click()
 
-    WebDriverWait(driver, 15).until(
-        EC.visibility_of_all_elements_located((By.XPATH, "//span[@email]"))
-    )
-
-    correos = driver.find_elements(By.XPATH, "//span[@email]")
-
-    correos_lista = [correo.get_attribute('email') for correo in correos]
+   
+    time.sleep(40)
+   
     
-    for correo in correos_lista:
-        print(correo)
 
 finally:
-    driver.quit()
+    driver.quit()  # Cerrar el navegador al finalizar
